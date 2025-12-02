b257b66d54ea3bae05ff3a9af2aa642fff8c00ca
khanreformiqo <momdou.khan@reformiqo.com>
2025-12-02 19:45:43 +0000

    update

--- notifier/notifier/doctype/whatsapp_message/whatsapp_message.py
+++ notifier/notifier/doctype/whatsapp_message/whatsapp_message.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 
 base_url = frappe.db.get_single_value("Evolution API Settings", "base_url")
 api_token = frappe.db.get_single_value("Evolution API Settings", "api_token")
+from notifier.api import send_media_message
 
 
 class WhatsAppMessage(Document):
@@ -18,6 +19,9 @@ class WhatsAppMessage(Document):
     def send_message(self):
         if self.content_type == "text":
             self.send_text_message()
+
+        else:
+            send_media_message(self.name)
        
     def send_text_message(self):
 
@@ -44,44 +48,4 @@ class WhatsAppMessage(Document):
             frappe.log_error(frappe.get_traceback(), "WhatsApp Message Error")
             frappe.throw(f"Failed to send WhatsApp message: {str(e)}")
 
-    def send_media_message(self):
-        try:
-            if not self.attach:
-                frappe.throw("Please attach a file to send media message")
-
-            extension = self.attach.split(".")[-1].lower()
-            mimetype = self.content_type + "/" + extension
-
-            # Get full URL for the attached file
-            # Ensure path starts with / if it doesn't already
-            file_path = (
-                self.attach if self.attach.startswith("/") else f"/{self.attach}"
-            )
-            media_url = frappe.utils.get_url(file_path)
-
-            url = f"{base_url}/message/sendMedia/{self.instance}"
-
-            payload = {
-                "number": self.to,
-                "mediatype": self.content_type,
-                "mimetype": mimetype,
-                "caption": self.message,
-                "media": media_url,
-                "fileName": self.label,
-                "linkPreview": True,
-            }
-            headers = {"apikey": api_token, "Content-Type": "application/json"}
-
-            response = requests.post(url, json=payload, headers=headers)
-            response.raise_for_status()  # Raise an exception for bad status codes
-
-            response_data = response.json()
-            frappe.log_error(response_data, "WhatsApp Message Error")
-            key_data = response_data.get("key")
-            if key_data:
-                self.message_id = key_data.get("id")
-
-            return response_data
-        except Exception as e:
-            frappe.log_error(frappe.get_traceback(), "WhatsApp Message Error")
-            frappe.throw(f"Failed to send WhatsApp media message: {str(e)}")
+    
