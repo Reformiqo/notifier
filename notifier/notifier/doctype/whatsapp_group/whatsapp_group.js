48e00236d9e82ffa850046203806d5754d1a0be3
khanmomodou101 <khanmomodou101@gmail.com>
2026-07-07 09:38:17 +0000

    update

---
+++ notifier/notifier/doctype/whatsapp_group/whatsapp_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, royalsmb and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("WhatsApp Group", {
+// 	refresh(frm) {
+
+// 	},
+// });
